2e9bb32c84415408c9df1d2f9ebf0b42ca277094
victorx2904 <victorvillarreta6@gmail.com>
2024-10-07 22:07:48 -0400

    changes in DB repared tenkiu,conichiwa

--- DB/elbarrildb.sql
+++ DB/elbarrildb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-10-2024 a las 04:04:14
+-- Tiempo de generación: 08-10-2024 a las 04:07:25
 -- Versión del servidor: 8.0.39
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `elbarrildb`
 --
+CREATE DATABASE IF NOT EXISTS `elbarrildb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `elbarrildb`;
 
 -- --------------------------------------------------------
 
